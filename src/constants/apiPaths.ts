93ba933554f53ffd819c90a1f893bf261c8a07b1
Tatiana Kuznetsova <tatiana_kuznetsova@epam.com>
2023-12-04 01:17:52 +0600

    add api path and ability to upload file in s3 bucket

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
-  product: "https://4y8wnbgpcc.execute-api.eu-west-1.amazonaws.com/prod/",
+  product: "https://4y8wnbgpcc.execute-api.eu-west-1.amazonaws.com/prod",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://zp16rkpg2b.execute-api.eu-west-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
